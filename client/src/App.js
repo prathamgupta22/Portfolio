4251a76823b3d86fb269108d9d7489641902349e
prathamgupta22 <pratham.guptapg01@gmail.com>
2023-07-15 18:30:09 +0530

    homedone

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { NavBar } from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
